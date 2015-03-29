59b9153094bd5dceb5ba153618d91a339e2dfece
Mark Tearle <mark@tearle.com>
2015-03-29 19:04:10 +0800

    Remove unnecessary code from example

--- EXAMPLE-npyscreenreactor.py
+++ EXAMPLE-npyscreenreactor.py
@@ -20,12 +20,10 @@ class MyTestApp(npyscreen.StandardApp):
 	self.sent = sent
 	self.received = received
 
-
     # socket code
     def onStart(self):
 	self.keypress_timeout_default = 1
-        self.addForm("MAIN",       MainForm, name="Screen 1", color="IMPORTANT",)
-        self.addForm("SECOND",     MainForm, name="Screen 2", color="WARNING",  )
+        self.addForm("MAIN",       MainForm, name="EXAMPLE-npyscreenreactor", color="IMPORTANT",)
 	self.sent=""
 	self.currentform = None
 
@@ -43,12 +41,9 @@ class MyTestApp(npyscreen.StandardApp):
 class MainForm(npyscreen.ActionForm):
     def create(self):
 	self.keypress_timeout_default = 1
-        self.add(npyscreen.TitleText, name = "Text:", value= "Press ^T to change screens" )
         self.sentfield = self.add(npyscreen.TitleText, name = "Sent:", value="", editable=False )
         self.receivedfield = self.add(npyscreen.TitleText, name = "Received:", value="", editable=False )
         
-        self.add_handlers({"^T": self.change_forms})
-
     def while_waiting(self):
 	self.sentfield.value = self.parentApp.sent
 	self.receivedfield.value = self.parentApp.received
@@ -58,15 +53,6 @@ class MainForm(npyscreen.ActionForm):
     def on_ok(self):
         # Exit the application if the OK button is pressed.
         self.parentApp.switchForm(None)
-
-    def change_forms(self, *args, **keywords):
-        if self.name == "Screen 1":
-            change_to = "SECOND"
-        else:
-            change_to = "MAIN"
-
-        # Tell the MyTestApp object to change forms.
-        self.parentApp.change_form(change_to)
 
 
 class Echo(protocol.Protocol):
@@ -81,7 +67,7 @@ class Echo(protocol.Protocol):
 	self.factory.app.updateFields(data,response)
      
     def connectionMade(self):
-	response = "Hello there ...\n"
+	response = "OK ... Hello there ...\n"
 	data = "Connection from " + str(self.transport.getPeer())
 
 	self.transport.write(response)
