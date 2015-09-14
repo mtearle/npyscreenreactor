414c5d0f9e7e92772cf65be976791889e96e2799
Mark Tearle <mark@tearle.com>
2015-09-14 21:49:58 +0800

    Package with setuptools

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Framework :: Twisted',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Natural Language :: English',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+setup(
+    name='npyscreenreactor',
+    version='1.1',
+    license='MIT',
+    classifiers=classifiers,
+    author='Mark Tearle',
+    author_email='mark@tearle.com',
+    description = 'Twisted reactor for npyscreen',
+    long_description = 'npyscreenreactor is a Twisted reactor for the npyscreen curses library',
+    url='https://github.com/mtearle/npyscreenreactor',
+    packages=find_packages(),
+    keywords=['npyscreen', 'twisted'],
+    install_requires=['twisted', 'npyscreen']
+)
