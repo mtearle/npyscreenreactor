87d897949f87d092e67ca90afefb908e107a9599
Mark Tearle <mark@tearle.com>
2016-01-30 10:30:07 +0800

    Commit changes for v1.3 release

---
+++ HISTORY.md
@@ -0,0 +1,8 @@
+History for npyscreenreactor
+============================
+
+v1.3
+----
+
+Add python 3 support and resolve unicode handling issues in examples
+
--- setup.py
+++ setup.py
@@ -17,13 +17,14 @@ classifiers = [
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
     'Natural Language :: English',
     'Topic :: Software Development :: Libraries :: Python Modules'
 ]
 
 setup(
     name='npyscreenreactor',
-    version='1.2',
+    version='1.3',
     license='MIT',
     classifiers=classifiers,
     author='Mark Tearle',
@@ -31,7 +32,7 @@ setup(
     description = 'Twisted reactor for npyscreen',
     long_description = 'npyscreenreactor is a Twisted reactor for the npyscreen curses library',
     url='https://github.com/mtearle/npyscreenreactor',
-    download_url='https://github.com/mtearle/npyscreenreactor/tarball/v1.2',
+    download_url='https://github.com/mtearle/npyscreenreactor/tarball/v1.3',
     packages=find_packages(),
     keywords=['npyscreen', 'twisted'],
     install_requires=['twisted', 'npyscreen']
